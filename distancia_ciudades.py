eb542ca8aa835575e94a2f9544958e6ba7a7a500
skoll43 <lukasofkonoha@gmail.com>
2023-07-02 03:04:44 +0000

    añadidas instrucciones

---
+++ distancia_ciudades.py
@@ -0,0 +1,11 @@
+'''
+Se solicita crear un script en Python que realice lo siguiente:
+● Mida la distancia entre una ciudad de Chile y una ciudad de algún otro país en Latinoamérica en kilómetros (kms).
+● Solicitar “Ciudad de Origen” y “Ciudad de Destino”, en español.
+● Mostar la duración del viaje en horas, minutos y segundos.
+● Mostrar el combustible requerido para el viaje en litros (lts)
+● Los valores deben tener solo 1 decimal.
+● Agregar como salida la letra S.
+● Que imprima la narrative del viaje.
+● El script debe ser subido al repositorio de GitHub con un commit a elección.
+''''''
